6033ae6968194916155344462c962e43eb2b3da9
widiand <widiand36@gmail.com>
2025-08-13 12:18:40 +0700

    test goroutine

---
+++ goroutine_test.go
@@ -0,0 +1,18 @@
+package belajargolanggoroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("ups")
+
+	time.Sleep(1 * time.Second)
+}
