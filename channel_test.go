60593cc2278739faf9d251386f02ffb9039d12a9
widiand <widiand36@gmail.com>
2025-08-15 20:41:15 +0700

    select channel

--- channel_test.go
+++ channel_test.go
@@ -23,7 +23,7 @@ func TestCreateChannel(t *testing.T) {
 
 func GiveMeResponse(channel chan string) {
 	time.Sleep(2 * time.Second)
-	channel <- "Eko"
+	channel <- "Eko Kurniawan Khannedy"
 }
 
 func TestChannelAsParameter(t *testing.T) {
@@ -90,4 +90,29 @@ func TestRangeChannel(t *testing.T) {
 		fmt.Println("menerima data", data)
 	}
 	fmt.Println("DONE")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <- channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <- channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
