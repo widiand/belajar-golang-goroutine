1bd062a0770fc7244d4465216f341572ad762837
widiand <widiand36@gmail.com>
2025-08-15 20:51:44 +0700

    default select channel

--- channel_test.go
+++ channel_test.go
@@ -116,3 +116,30 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <- channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <- channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("menunggu data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
