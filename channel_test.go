f9cd67bc7b9d620e2b7ec881da4bdb299410b34f
widiand <widiand36@gmail.com>
2025-08-15 20:15:38 +0700

    buffered channel

--- channel_test.go
+++ channel_test.go
@@ -54,3 +54,24 @@ func TestInOutChannel(t *testing.T) {
 	time.Sleep(3 * time.Second)
 	close(channel)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Eko"
+		channel <- "Kurniawan"
+		channel <- "Khannedy"
+	}()
+
+	go func() {
+		fmt.Println(<- channel)
+		fmt.Println(<- channel)
+		fmt.Println(<- channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("selesai")
+}
+}
