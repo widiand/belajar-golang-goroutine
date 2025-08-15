38339d9e60809f2c39982bea161eb74c8ea7e349
widiand <widiand36@gmail.com>
2025-08-15 20:19:32 +0700

    range channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package belajargolanggoroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -74,4 +75,19 @@ func TestBufferedChannel(t *testing.T) {
 	time.Sleep(2 * time.Second)
 	fmt.Println("selesai")
 }
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for	i := 0; i < 10; i++ {
+			channel <- "perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("menerima data", data)
+	}
+	fmt.Println("DONE")
 }
