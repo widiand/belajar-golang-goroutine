0a967a7e708fc5071a00c068f900381f878727ae
widiand <widiand36@gmail.com>
2025-08-18 18:20:09 +0700

    gomaxprocs

---
+++ gomaxprocs_test.go
@@ -0,0 +1,54 @@
+package belajargolanggoroutine
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGetGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCPU := runtime.NumCPU()
+	fmt.Println("Total CPU ", totalCPU)
+	
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread ", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine ", totalGoroutine)
+
+	group.Wait()
+}
+
+func TestChangeThreadNumber(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCPU := runtime.NumCPU()
+	fmt.Println("Total CPU ", totalCPU)
+	
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread ", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine ", totalGoroutine)
+
+	group.Wait()
+}
